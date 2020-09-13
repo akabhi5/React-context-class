8e66883d3dcb422327d4de74d4dc0dc05d9c2b9f
Abhishek Kumar <abhishekk580@gmail.com>
2020-09-13 15:54:25 +0530

    first commit

---
+++ src/components/BookList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
+class BookList extends Component {
+  static contextType = ThemeContext;
+
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div
+        className="book-list"
+        style={{ background: theme.bg, color: theme.syntax }}
+      >
+        <ul>
+          <li style={{ background: theme.ui }}>the way of kings</li>
+          <li style={{ background: theme.ui }}>the name of the wind</li>
+          <li style={{ background: theme.ui }}>the final empire</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+/*
+thsi approach of cnsumer can also be used in functional based
+components and multple contexts can be used by this approach.
+*/
+
+export default BookList;
